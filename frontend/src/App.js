e796bc2b02952b63b4d150de6b57a95631ab96be
choo <choo.hacks@gmail.com>
2019-12-08 22:08:12 +0900

    Add frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,77 +1,124 @@
 import React from 'react';
 
 /* import material UI components */
-import { Container } from '@material-ui/core';
-import Typography    from '@material-ui/core/Typography';
+import { Container }  from '@material-ui/core';
+import Grid           from '@material-ui/core/Grid';
 
 /* import third party libraries */
 import axios from 'axios';
 
 /* import components */
 import Header from './components/Header';
-import KeywordSuggest from './components/KeywordSuggest';
+import HandwritingCanvas from './components/HandwritingCanvas';
+import ResultsModal  from './components/ResultsModal';
+
+
+
+const AJAX_URL = '/api/predict';
+const NUM_DISPLAY = 4;
 
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      params: {
-        query: '',
-      },
+      currentStatus: 0, // 0: writing, 1: selecting, 2: showing score
       ajaxResults: {
-        list: [],
+        predicted: [],
+      },
+      imageBlob: null,
+      selected: {
+        kana: '',
+        score: '',
+        prob: '',
       },
     }
+    this.showResults = this.showResults.bind(this);
+    this.closeResults = this.closeResults.bind(this);
+    this.selectKana = this.selectKana.bind(this);
+
   }
 
-  updateQuery(val) {
-    this.updateParam({query: val})
+  showResults(image_blob) {
+    this.setState({
+      currentStatus: 1,
+      imageBlob: image_blob
+    });
+    const form = new FormData();
+    form.append('uploadfile', image_blob)
+    const config = {
+      headers: { 'content-type': 'multipart/form-data' }
+    };
+    axios.post(AJAX_URL, form, config)
+      .then(response => {
+        const resData = response.data;
+        this.setState({
+          ajaxResults: {
+            predicted: resData.predicted,
+          },
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
-  updateParam(params) {
-    this.setState({params: params});
-    const newParams = {...this.state.params, ...params};
-    axios.get('/api/sample', {
-      params: newParams
-    })
-      .then(
-        (result) => {
-          const resultData = result.data;
-          this.setState({
-            ajaxResults: {
-              list: resultData,
-            },
-          });
-        }
-      )
-      .catch(
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+  closeResults() {
+    this.setState({currentStatus: 0, ajaxResults: {predicted: []}});
+  }
+
+  selectKana(kanaCode) {
+    let prob = 0.0;
+    for (const [kana, p] of this.state.ajaxResults.predicted) {
+      if (kana === kanaCode) {
+        prob = p;
+        break;
+      }
+    }
+    // 1 - ( x - 1)**4
+    // 1 - (-x + 1)**3
+    // 1 - ( x - 1)**2
+    //const score = (1.0 -  (prob - 1) ** 4) * 100.0;
+    //const score = (1.0 - (-prob + 1) ** 3) * 100.0;
+    //const score =  (- ((1.0 - prob) ** (1 / 3) - 1)) * 100.0
+    const score =  (-((1.0 - prob) ** (1/2)) + 1) * 100.0
+    this.setState({
+      currentStatus: 2,
+      selected: {
+        kana: String.fromCharCode(parseInt(kanaCode, 16)),
+        //prob: prob.toFixed(4),
+        prob: prob,
+        score: score.toFixed(2),
+      },
+    });
   }
 
   render() {
     return (
       <div>
         <Header />
-        <Container maxWidth='md'>
-          <div>
-            <KeywordSuggest
-              keyword={this.state.params.query}
-              suggests={this.state.ajaxResults.list}
-              onChange={(query) => this.updateQuery(query)}
+        <Container
+          maxWidth='xs'
+        >
+          <Grid container justify = "center">
+            <HandwritingCanvas
+              onUpdateCanvas={this.showResults}
             />
-            <Typography>{this.state.params.query}</Typography>
-          </div>
+            <ResultsModal
+              currentStatus={this.state.currentStatus}
+              close={this.closeResults}
+              predicted={this.state.ajaxResults.predicted.slice(0, NUM_DISPLAY)}
+              imageBlob={this.state.imageBlob}
+              onSelectKana={this.selectKana}
+              selected={this.state.selected}
+            />
+          </Grid>
         </Container>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ frontend/src/components/ResultsSelection.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Box            from '@material-ui/core/Box';
+import Button         from '@material-ui/core/Button';
+import Grid           from '@material-ui/core/Grid';
+import TextField      from '@material-ui/core/TextField';
+import Typography     from '@material-ui/core/Typography';
+
+import TitleWithImage from './TitleWithImage';
+import {isKana, shuffleArray} from '../utils';
+
+const ResultsSelection = props => {
+  return (
+    <>
+      <TitleWithImage
+        //left=Wording.predicted[LANG]
+        left={'written letter '}
+        right={'is ...'}
+        imageBlob={props.imageBlob}
+        leftWidth={5}
+      />
+
+      <Box>
+      {shuffleArray(props.predicted).map(result => {
+        const kanaCode = result[0];
+        return (
+          <Button
+            key={kanaCode}
+            variant="contained"
+            onClick={(e) => props.onSelectKana(kanaCode)}
+          >
+            {String.fromCharCode(parseInt(kanaCode, 16)) + '?'}
+            {' ' + result[1].toFixed(4)} {/* probablity */}
+          </Button>
+        )
+      })}
+      </Box>
+
+      <InputCustom
+        onSelectKana={props.onSelectKana}
+      />
+    </>
+  );
+};
+
+
+const InputCustom = props => {
+  const [isError, setIsError] = React.useState(false);
+  const [customChar, setCustomChar] = React.useState('');
+  const onChangeText = (text) => {
+    console.log('on change text')
+    setCustomChar(text);
+  };
+  const submitCustomChar = () => {
+    console.log(' submit hogheoge');
+    if (!customChar.length) {
+      setIsError(false);
+    } else {
+      if (customChar.length === 1 && isKana(customChar)) {
+        const code = '0x' + customChar.charCodeAt(0).toString(16)
+        props.onSelectKana(code)
+        setIsError(false);
+      } else {
+        setIsError(true);
+      }
+    }
+  };
+  return (
+    <Grid container direction="row" justify="flex-end" alignItems="baseline">
+      <Typography>{' or ...  '}</Typography>
+      {/*<Typography>{Wording.otherLetter[LANG]}</Typography>*/}
+      <TextField
+        placeholder=""
+        margin="normal"
+        variant="outlined"
+        // TODO: should use makeStyles 
+        style={{width: 120, textAlign: 'center'}} 
+        helperText={isError ? 'かな一字を入力': ''}
+        onChange={e => onChangeText(e.target.value)}
+        inputProps={{maxLength: 4}}
+      />
+      <Button
+        variant="contained"
+        onClick={e => submitCustomChar()}
+      >{'submit this char'}</Button>
+    </Grid>
+  );
+};
+
+export default ResultsSelection;
