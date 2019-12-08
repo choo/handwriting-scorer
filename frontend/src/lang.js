0394f1132b6ac90aecb1af99d950d517d19bede3
choo <sugimori.choo@gmail.com>
2019-12-08 22:08:12 +0900

    Add frontend files

---
+++ frontend/src/lang.js
@@ -0,0 +1,21 @@
+const Wording = {
+  aboveCanvas: {
+    en: 'Write Kana !',
+    ja: 'かなを書いて',
+  },
+  scoreButton: {
+    en: 'Score',
+    ja: '文字を採点',
+  },
+  resetButton: {
+    en: 'Reset',
+    ja: 'リセット',
+  },
+
+  predicted: {
+    en: 'is ... ',
+    ja: 'は …',
+  },
+};
+
+export default Wording;
