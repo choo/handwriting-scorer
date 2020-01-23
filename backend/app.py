6bfb2f7dfc818e9567df218706d793e2e0f0457c
choo <sugimori.choo@gmail.com>
2020-01-23 23:45:39 +0900

    Add functionality to bake cookie

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,34 @@
 #! /usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-import os, argparse
+import os, argparse, datetime, secrets
 from flask import Flask, jsonify, send_from_directory, request, make_response
 from models.model import Model
 
 
+SESSION_COOKIE = '__uid'
 ROOT_DIR = './build'
 STATIC_DIR = os.path.join(ROOT_DIR, 'static')
 app = Flask(__name__, static_folder=STATIC_DIR)
 
 model = Model()
 model.setup()
+
+
+def _make_token(num_bytes=16):
+    """
+    Creates a cryptographically-secure, URL-safe string (for Python 3.6+)
+    """
+    return secrets.token_urlsafe(num_bytes)
+
+
+def _get_session_id(cookie_name):
+    if cookie_name in request.cookies:
+        session_id = request.cookies.get(cookie_name)
+    else:
+        session_id = _make_token()
+    return session_id
+
 
 def _extract_uploaded_image():
     if 'uploadfile' not in request.files:
@@ -46,7 +63,15 @@ def predictResult():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return send_from_directory(ROOT_DIR, 'index.html')
+    session_id = _get_session_id(SESSION_COOKIE)
+    res = send_from_directory(ROOT_DIR, 'index.html')
+
+    max_age = 60 * 60 * 24 * 365 * 50 # appox.) 50 years
+    expires = int(datetime.datetime.now().timestamp()) + max_age
+    res.set_cookie(SESSION_COOKIE, value=session_id,
+            max_age=max_age, expires=expires, path='/',)
+
+    return res
 
 
 if __name__ == "__main__":
