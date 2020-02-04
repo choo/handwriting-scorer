af2428912e716b77a1928ffe347f6b6fd87ab80f
choo <sugimori.choo@gmail.com>
2020-02-05 07:02:02 +0900

    Add comment

--- preact/src/components/container/index.js
+++ preact/src/components/container/index.js
@@ -1,6 +1,9 @@
 import { h } from 'preact';
 import style from './style';
 
+/**
+ * component only for centering
+ */
 const Container = (props) => (
   <div class={style.container} style={props.style}>
     {props.children}
