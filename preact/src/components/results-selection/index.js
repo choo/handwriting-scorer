465bddd38bc06c6f70b4c72fd2e2e786e3b1792f
choo <sugimori.choo@gmail.com>
2020-02-08 20:57:32 +0900

    Add resultSelection component

---
+++ preact/src/components/results-selection/index.js
@@ -0,0 +1,149 @@
+import { useState } from 'preact/hooks';
+import {isKana, shuffleArray} from '../../utils/utils';
+import {hiragana, katakana, alphaNum} from '../../utils/const';
+
+import Grid from '../grid';
+import Button from '../button';
+
+const ResultsSelection = props => {
+  const [isSuggesting, setSuggesting] = useState(true);
+  const setCustom = () => {
+    setSuggesting(false);
+  }
+  return (
+    <>
+      <Grid container m='4px 0 0' alignItems='center'>
+        <Grid p='0 6px 0 0' flex={1}>
+          <img style={{width: '100%'}}
+            src={window.URL.createObjectURL(props.imageBlob)}
+            border={'1'}
+            alt={'canvas content'}
+          />
+        </Grid>
+        <Grid flex={2}>
+          <span>{'書いた文字は…'}</span>
+        </Grid>
+      </Grid>
+
+      {isSuggesting ? (
+        <SelectSuggestion
+          predicted={props.predicted}
+          onSelectKana={props.onSelectKana}
+          onClickBack={props.onClickBack}
+          onSelectCustom={setCustom}
+        />
+      ) : (
+        <SelectCustom
+          onSelectKana={props.onSelectKana}
+          onClickBack={props.onClickBack}
+        />
+      )}
+      <Grid m='12px 0 16px'>
+        <Button outlined onClick={props.onClickBack}>戻る</Button>
+      </Grid>
+    </>
+  );
+};
+
+
+const SelectSuggestion = props => {
+  return (
+    <>
+      <Grid container alignItems='center'>
+      {shuffleArray(props.predicted).map(result => {
+        const kanaCode = result[0];
+        return (
+          <Grid flex={1/6} m='2px' key={kanaCode}>
+            <Button
+              outlined
+              style={{minWidth: '50px'}}
+              onClick={(e) => props.onSelectKana(kanaCode)}
+            >
+              <span>
+              {String.fromCharCode(parseInt(kanaCode, 16))}
+              {/* {' ' + result[1].toFixed(4)} {/* probablity */}
+              </span>
+            </Button>
+          </Grid>
+        )
+      })}
+      </Grid>
+
+      <Grid container justify='flex-end'  alignItems="flex-end" m='4px 0'>
+        <Button outlined
+          onClick={props.onSelectCustom}
+        >他の文字から選ぶ</Button>
+      </Grid>
+    </>
+  );
+};
+
+
+const SelectCustom = props => {
+  const [charType, setCharType] = useState('hira');
+  const TYPES = [
+    {display: 'ひら',   val: 'hira'},
+    {display: 'カタ',   val: 'kata'},
+    {display: '漢字',   val: 'kanji'},
+    {display: '英数字', val: 'alpha'},
+  ];
+  const CHARS = {
+    'hira' : hiragana,
+    'kata' : katakana,
+    'kanji': [],
+    'alpha': alphaNum,
+  };
+  return (
+    <>
+      <Grid container style={{
+        //boxShadow: '0 2px 2px 0 rgba(0,0,0,0.14), ' +
+        //           '0 3px 1px -2px rgba(0,0,0,0.12), ' +
+        //           '0 1px 5px 0 rgba(0,0,0,0.2)',
+        border: '1px solid',
+        height: '36px',
+      }}>
+        {TYPES.map(t => (
+          <Grid flex={1}>
+            <button onClick={() => setCharType(t.val)}
+              style={{
+                width: '100%',
+                height: '100%',
+                //border: '1px solid',
+                backgroundColor: charType === t.val ? '#ffc2e3' : '#fff',
+                fontSize: '14px',
+              }}
+            >
+              {t.display}
+            </button>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Grid p='4px 12px 4px'>
+        {CHARS[charType].map((row, i) => {
+          return (
+          <Grid container key={i} justify='space-between'>
+            {row.map((c, j) => {
+              const kanaCode = '0x' + c.charCodeAt(0).toString(16)
+              return (
+                <Grid flex={1/6} p='2px 3px' key={j}>
+                  {c ? (
+                    <Button outlined
+                      style={{minWidth:'48px'}}
+                      onClick={(e) => props.onSelectKana(kanaCode)}
+                    >
+                      <span>{c}</span>
+                    </Button>
+                  ) : null}
+                </Grid>
+              )
+            })}
+          </Grid>
+          )
+        })}
+      </Grid>
+    </>
+  );
+};
+
+export default ResultsSelection;
