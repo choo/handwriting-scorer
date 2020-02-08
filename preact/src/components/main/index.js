a17850d426112b265c8245ae7b236a43817e5990
choo <choo.hacks@gmail.com>
2020-02-08 20:57:32 +0900

    Add resultSelection component

--- preact/src/components/main/index.js
+++ preact/src/components/main/index.js
@@ -4,6 +4,8 @@ import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
 import {postImage} from '../../utils/ajax';
 
 import HandwritingCanvas from '../handwriting-canvas';
+import ResultsSelection from '../results-selection';
+import {NUM_DISPLAY} from '../../utils/const';
 
 const STATUS = {
   WRITING: 0,
@@ -16,7 +18,7 @@ disableBodyScroll(window.document.body);
 const Main = () => {
   const [status, setStatus] = useState(STATUS.WRITING);
   const [imageBlob, setImageBlob] = useState(null);
-  const [predicted, setPredicted] = useState(null);
+  const [predicted, setPredicted] = useState([]);
   const [selected, setSelected] = useState({});
 
   const submitImage = async (imgBlob) => {
@@ -28,12 +30,28 @@ const Main = () => {
       setPredicted(result.predicted);
     }
   };
+  const goBackHome = () => {
+    setStatus(STATUS.WRITING);
+    disableBodyScroll(window.document.body);
+    setPredicted([]);
+  };
+
 
   return (
     <div>
-      <HandwritingCanvas
-        onScore={submitImage}
-      />
+      {status === STATUS.WRITING ? (
+        <HandwritingCanvas
+          onScore={submitImage}
+        />
+      ) : status === STATUS.SELECTING ? (
+        <ResultsSelection
+          predicted={predicted.slice(0, NUM_DISPLAY)}
+          imageBlob={imageBlob}
+          onSelectKana={selectKana}
+          onClickBack={goBackHome}
+        />
+      ) : (
+      )}
     </div>
   );
 };
