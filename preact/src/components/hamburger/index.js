428a3e5728fbd3d254d34aaaba8471d2b551afae
choo <sugimori.choo@gmail.com>
2020-02-20 23:54:48 +0900

    Extract hambuger menu component

---
+++ preact/src/components/hamburger/index.js
@@ -0,0 +1,32 @@
+import { useState } from 'preact/hooks';
+import style from './style.css';
+
+const Hamburger = (props) => {
+  const [isActive, setActive] = useState(false);
+  const toggleMenu = () => {
+    setActive(!isActive);
+  };
+  return (
+    <>
+      <button type='button'
+        class={
+          style.hamburger + " " +
+          style.hamburgerSlider + " " +
+          (isActive ? style.isActive : '')
+        }
+        onClick={toggleMenu}
+      >
+        <span class={style.hamburgerBox}>
+          <span class={style.hamburgerInner}></span>
+        </span>
+      </button>
+      <div class={`${style.menuWrapper} ${isActive ? style.isActive : ''}`}
+          style={{top: props.height}}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+export default Hamburger;
