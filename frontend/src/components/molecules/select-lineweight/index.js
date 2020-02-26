ef9619b1e8c9b42d1d78e2316500b0ef1c624adc
choo <choo.hacks@gmail.com>
2020-02-26 23:39:09 +0900

    Add functionality to select line weight

---
+++ frontend/src/components/molecules/select-lineweight/index.js
@@ -0,0 +1,58 @@
+import Popup from '../../atoms/popup';
+
+import Wording   from '../../../utils/lang';
+
+const LANG = 'ja'
+const WEIGHT_OPTIONS = [2,5,10,15,20,25,30];
+
+const SelectLineWeight = (props) => {
+  const handleClickItem = val => {
+    props.onChangeWeight(val);
+  };
+
+  /* buttonText=<LineWeightIcon /> */
+  return (
+    <>
+      <Popup
+        style={{
+          bottom: '2.0rem',
+          transformOrigin: 'bottom left',
+        }}
+        onClickClose={true}
+        buttonText={Wording.lineWeightButton[LANG]}
+        menu={
+          <div style={{
+            minWidth: '160px',
+            overflow: 'auto',
+            boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.2)',
+            backgroundColor: '#fff',
+            margin: '4px 0px',
+          }}>
+            {
+              WEIGHT_OPTIONS.map(val => {
+                // FIXME: add checked icon
+                return (
+                  <a key={val}
+                    selected={val === props.weight}
+                    onClick={() => handleClickItem(val)}
+                    style={{
+                    color: '#000',
+                    display: 'block',
+                    padding: '10px',
+                    textDecoration: 'none',
+                    cursor: 'pointer',
+                    borderBottom: '1px solid #bbb',
+                  }}>
+                    {val}px
+                  </a>
+                )
+              })
+            }
+          </div>
+        }
+      />
+    </>
+  );
+};
+
+export default SelectLineWeight;
