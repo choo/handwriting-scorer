eff72e2bb1d8461e19bfc85a76ac7764f3ef6926
choo <choo.hacks@gmail.com>
2020-03-13 21:59:14 +0900

    Change wording of hambuger menu, and extract styling to css

--- frontend/src/components/molecules/header/index.js
+++ frontend/src/components/molecules/header/index.js
@@ -1,6 +1,7 @@
 import Logo from '../../../assets/logotype_01.png'
 import Hambuger from '../../atoms/hamburger';
 import {HEADER_HEIGHT, MAX_WIDTH} from '../../../utils/layout';
+import style from './style.css';
 
 const Header = (props) => {
   const height = `${HEADER_HEIGHT}px`
@@ -21,35 +22,9 @@ const Header = (props) => {
           overflow: 'auto',
           boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.2)',
         }}>
-          <a href="/" onClick={props.goToMain} style={{
-            color: '#000',
-            display: 'block',
-            padding: '20px',
-            textDecoration: 'none',
-            cursor: 'pointer',
-            borderBottom: '1px solid #bbb',
-          }}>
-            Top
-          </a>
-          <a href="/record" style={{
-            color: '#000',
-            display: 'block',
-            padding: '20px',
-            textDecoration: 'none',
-            cursor: 'pointer',
-            borderBottom: '1px solid #bbb',
-          }}>
-            My Records
-          </a>
-          <a href="/about" style={{
-            color: '#000',
-            display: 'block',
-            padding: '20px',
-            textDecoration: 'none',
-            cursor: 'pointer',
-          }}>
-            About
-          </a>
+          <a href="/" onClick={props.goToMain} class={style.item}>トップ</a>
+          <a href="/record" class={style.item}>採点実績</a>
+          <a href="/about" class={style.item}>サービスについて</a>
         </div>
       </Hambuger>
     </header>
