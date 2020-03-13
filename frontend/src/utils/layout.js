d4d67d6f537945d414d9d712dde8543192b9230d
choo <choo.hacks@gmail.com>
2020-03-14 06:18:21 +0900

    Add function to getActiveColor

--- frontend/src/utils/layout.js
+++ frontend/src/utils/layout.js
@@ -2,3 +2,6 @@ export const MAX_WIDTH = 480; // px
 export const WINDOW_PADDING = 15; // px
 export const CANVAS_BORDER = 2;
 export const HEADER_HEIGHT = 60;
+export const getActiveColor = (val, current) => (
+  current === val ? '#ffb6b6' : '#fff'
+);
